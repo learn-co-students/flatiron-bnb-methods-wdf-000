3fcf11bb501f32e4c042a5b39bfecd54d70ee7e2
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-31 16:49:38 -0400

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,27 @@ class Listing < ActiveRecord::Base
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
   has_many :reservations
-  has_many :reviews, :through => :reservations
+  has_many :reviews, :through => :reservations 
   has_many :guests, :class_name => "User", :through => :reservations
+  validates :address, presence: true
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :neighborhood, presence: true
+  validates :listing_type, presence: true
+
+  before_create :change_host_status
+  after_destroy :check_and_change_host_status_on_destroy
+
+  def average_review_rating
+    reviews.map {|review| review.rating}.reduce(:+) / reviews.count.to_f
+  end
   
+  def change_host_status
+    self.host.update(host: true)
+  end
+
+  def check_and_change_host_status_on_destroy
+    self.host.update(host: false) if self.host.listings.count == 0
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,13 @@
 class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
+  validates :description, presence: true
+  validates :rating, presence: true
+  validate :reservation_is_valid_accepted_and_ended
 
+  def reservation_is_valid_accepted_and_ended
+    unless reservation && reservation.status == "accepted" && reservation.checkout < Time.now
+      errors.add(:reservation, "Reservation is not valid, has not been accepted, or has not yet reached checkout")
+    end
+  end
 end
