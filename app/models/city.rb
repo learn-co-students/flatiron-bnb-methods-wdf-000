46b054abb7a5d77279d00ba2aece4a4fe8de8ccf
aehmt <calisahmet@gmail.com>
2016-08-24 15:26:40 -0400

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,7 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
-
+  has_many :reservations, through: :listings
+  include Concerns::InstanceMethods
+  extend Concerns::ClassMethods
 end
-
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,38 @@ class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
   has_one :review
+  validates :checkin, :checkout, presence: true
+  validate :guest_and_host_not_the_same, :checkout_after_checkin, :listing_available?
 
+  def duration
+    checkout-checkin
+  end
+
+  def total_price
+    duration*listing.price
+  end
+
+  private
+
+  def guest_and_host_not_the_same
+    if guest_id == listing.host_id
+      errors.add(:guest_id, "You can't book your own apartment.")
+    end
+  end
+
+  def checkout_after_checkin 
+    if checkout && checkin && checkout <= checkin
+      errors.add(:guest_id, "Your check-out date needs to be after your check-in.")
+    end
+  end
+
+  def listing_available?
+    Reservation.where(listing_id: listing.id).where.not(id: id).each do |r|
+      # binding.pry
+      booked_dates = r.checkin..r.checkout
+      if booked_dates === checkin || booked_dates === checkout
+        errors.add(:guest_id, "Sorry, this place isn't available during your requested dates.")
+      end
+    end
+  end
 end
