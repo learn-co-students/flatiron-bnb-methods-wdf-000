5eb183b0f104934e3d8ff2c93d467f9004f3a887
ozPop <ozziepopovas@gmail.com>
2016-09-07 17:40:36 -0400

    add city_openings method

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,20 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  # collects listings that dont have conflicts
+  def city_openings(start_date, end_date)
+    self.listings.collect do |listing|
+      listing if reservation_check(listing, start_date, end_date).all? {|r| r == nil}
+    end
+  end
+
+  private
+
+  def reservation_check(listing, start_date, end_date)
+    listing.reservations.collect do |r|
+      r if (Date.parse(start_date) <= r.checkout) and (Date.parse(end_date) >= r.checkin)
+    end
+  end
+
 end
 
