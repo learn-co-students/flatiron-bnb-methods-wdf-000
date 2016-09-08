815c193aba6f412200250a1bbcb9965eb1f7d1fa
fidelscodes <fidelscodes@gmail.com>
2016-09-08 10:52:14 -0400

    add listing validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,7 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates_presence_of :title, :description, :address, :listing_type, :price
+  validates_presence_of :neighborhood, numericality: { only_integer: true}
 end
