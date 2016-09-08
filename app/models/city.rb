f72772789c9754cdd465b8d2fcca1e407fb1f6a6
fidelscodes <fidelscodes@gmail.com>
2016-09-08 10:26:32 -0400

    add association for reservations through listings, commented out

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,7 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  # has_many :reservations, through: :listings
 
   def self.highest_ratio_res_to_listings
     ratios = {}
