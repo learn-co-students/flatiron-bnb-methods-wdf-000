f85d8846b8f2bf77ffc51ff1a2b805ad49860451
ozPop <ozziepopovas@gmail.com>
2016-09-08 19:06:37 -0400

    add toggle_host callback method

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,4 +7,18 @@ class Listing < ActiveRecord::Base
   
   validates_presence_of :address, :listing_type, :title, :description, :price, :neighborhood
 
+  after_save :toggle_host
+  after_destroy :toggle_host
+
+  private
+
+  def toggle_host
+    u = User.find_by(id: self.host_id)
+    if u.host && u.listings.empty?
+      u.update(host: false)
+    else
+      u.update(host: true)
+    end
+  end
+
 end
