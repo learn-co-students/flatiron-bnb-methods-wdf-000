8dc576cab8be56323b03aaf251c7c2186dcaaaa8
fidelscodes <fidelscodes@gmail.com>
2016-09-08 19:06:44 -0400

    add callback methods

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,4 +7,19 @@ class Listing < ActiveRecord::Base
 
   validates_presence_of :title, :description, :address, :listing_type, :price
   validates_presence_of :neighborhood, numericality: { only_integer: true}
+
+  after_save :toggle_host
+  after_destroy :toggle_host
+
+  private
+
+  def toggle_host
+    user = User.find_by(id: self.host_id)
+    if user.host && user.listings.empty?
+      user.update(host: false)
+    else
+      user.update(host: true)
+    end
+  end
+
 end
