40becfa1b9152758667d3b042400dd8ad5c7206c
fidelscodes <fidelscodes@gmail.com>
2016-09-09 12:20:17 -0400

    fix logic in Checkable.rb class methods pass tests for Neighborhood

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,39 +1,16 @@
 class City < ActiveRecord::Base
   include Checkable::InstanceMethods
+  extend Checkable::ClassMethods
 
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
   # has_many :reservations, through: :listings
-
-  def self.highest_ratio_res_to_listings
-    ratios = {}
-    self.all.each do |city|
-      ratios[city] = reservations(city).to_f / city.listings.count
-    end
-    ratios.max_by {|k, v| v}.first
-  end
-
-  def self.most_res
-    res_hash = {}
-    City.all.each do |city|
-      res_hash[city] = reservations(city)
-    end
-    res_hash.max_by {|k,v| v}.first
-  end
 
   # collects listings that dont have conflicts
   def city_openings(start_date, end_date)
     self.listings.collect do |listing|
       listing if reservation_check(listing, start_date, end_date).all? {|r| r == nil}
     end
-  end
-
-  private
-
-  # do class methods only work with other class methods?
-
-  def self.reservations(location)
-    location.listings.collect {|l| l.reservations.count}.reduce(:+)
   end
 
 end
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,7 +1,14 @@
 class Neighborhood < ActiveRecord::Base
   include Checkable::InstanceMethods
+  extend Checkable::ClassMethods
 
   belongs_to :city
   has_many :listings
+
+  def neighborhood_openings(start_date, end_date)
+    self.listings.collect do |listing|
+      listing if reservation_check(listing, start_date, end_date).all? {|r| r == nil}
+    end
+  end
 
 end
