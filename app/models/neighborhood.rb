b58deaa24094bfa5a50ca7342d4758a2715b7ee9
ozPop <ozziepopovas@gmail.com>
2016-09-09 10:46:43 -0400

    add #neighborhood_openings to neighborhood.rb"

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,13 @@
 class Neighborhood < ActiveRecord::Base
+  include Checkable::InstanceMethods
   belongs_to :city
   has_many :listings
+
+  # collects listings that dont have conflicts
+  def neighborhood_openings(start_date, end_date)
+    self.listings.collect do |listing|
+      listing if reservation_check(listing, start_date, end_date).all? {|r| r == nil}
+    end
+  end
 
 end
