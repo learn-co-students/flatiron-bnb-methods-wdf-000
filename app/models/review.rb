ad32ef1020f0d51c18aba3d8da0ccb4c20a8ca84
fidelscodes <fidelscodes@gmail.com>
2016-09-10 20:48:55 -0400

    add hosts, guests, host_review methods

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,11 +7,9 @@ class Review < ActiveRecord::Base
 
   def reservation_accepted_and_passed?
     if reservation.nil?
-      # binding.pry
       errors.add(:guest, "cannot leave a review on a nonexistent reservation")
     elsif reservation.status == "pending" || reservation.checkout > Date.today
       errors.add(:rating, "can only be left on an accepted reservation, after the checkout date")
     end
   end
-
 end
