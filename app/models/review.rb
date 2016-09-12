643d700ed8c76dcc543eb4c468f4ffa99f793e63
ozPop <ozziepopovas@gmail.com>
2016-09-12 14:57:50 -0400

    add validations to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,13 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates_presence_of :description, :rating, :reservation_id
+  validate :checked_out?
+  
+  def checked_out?
+    if !self.reservation.blank? && self.reservation.checkout > Date.today
+        errors.add(:review, "error: reservation must be checked out")
+    end
+  end
+
 end
