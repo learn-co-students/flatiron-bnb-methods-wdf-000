9d945ba1be9fe32cec4c5d02181e6c9ec55d0708
ozPop <ozziepopovas@gmail.com>
2016-09-12 14:58:22 -0400

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
-  has_many :listings, :foreign_key => 'host_id'
-  has_many :reservations, :through => :listings
-  has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
-  has_many :reviews, :foreign_key => 'guest_id'
-  
+  has_many :listings, foreign_key: :host_id
+  has_many :reservations, through: :listings
+  has_many :trips, foreign_key: :guest_id, :class_name => "Reservation"
+  has_many :reviews, foreign_key: :guest_id
+  # as a host, knows about the guests its had
+  has_many :guests, through: :reservations
+  # as a guest, knows about the hosts its had
+  has_many :rents, through: :trips, source: :listing
+  has_many :hosts, through: :rents
+  # as a host, knows about its reviews from guests
+  has_many :host_reviews, through: :guests, source: :reviews
 end
