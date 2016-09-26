0ff5e3012655d9e40abfcbb8b0a927fab2d199bf
Wendy Xu <guangw.xu@gmail.com>
2016-09-26 17:58:01 -0400

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,23 @@
 class City < ActiveRecord::Base
+  include Sharedmethod
+
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, :through => :listings
+  
+  def city_openings(startdate, enddate)
+    open_listings = []
+
+    self.listings.each do |listing|
+
+      open_listings << listing unless listing.reservations.any?{|reservation| (Date.parse(startdate) <= reservation.checkout) && (Date.parse(enddate) >= reservation.checkin)}
+
+    end
+    
+    open_listings
+  end
+
+
 
 end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,17 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates :rating, :description, presence: true
+  validate :res_not_accepted_res_not_passed
+
+  def res_not_accepted_res_not_passed
+    if self.reservation_id == nil
+      errors.add(:id, "can't be reviewed without a reservation")
+    elsif self.reservation.status != "accepted"
+      errors.add(:id, "can't be reviewed while reservation has not been accepted.")
+    elsif self.reservation.checkout > Date.today
+      errors.add(:id, "can't be reviewed before checkout.")      
+    end
+  end
+
 end
