8703afb794aca06805a85c001c06fae52c81605f
Dmitriy Khait <khaitdmitriy@gmail.com>
2016-09-27 15:36:13 -0400

    Done.

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,45 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
 
+  def neighborhood_openings(startdate, enddate)
+    answer = []
+    self.listings.each do |listing|
+      if !listing.reservations.any?{|reservation| (Date.parse(startdate) <= reservation.checkout) && (Date.parse(enddate) >= reservation.checkin)}
+        answer << listing
+      end
+    end
+    answer
+  end
+
+  def self.highest_ratio_res_to_listings
+    max = 0
+    answer = self.first
+     self.all.each do |city|
+       list_size = city.listings.size
+       city.listings.each do |listing|
+          if listing.reservations.size/list_size > max
+            max = listing.reservations.size/list_size
+            answer = city
+          end
+       end
+     end
+     answer
+  end
+
+  def self.most_res
+    max = 0
+    answer = self.first
+    self.all.each do |city|
+      city.listings.each do |listing|
+        if listing.reservations.size > max
+          max = listing.reservations.size
+          answer = city
+        end
+      end
+    end
+    answer
+  end
+
+
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,17 @@
 class Review < ActiveRecord::Base
+  validates :rating, :description, :reservation_id, presence: true
+  validate :res_not_accepted_res_not_passed
+
+ def res_not_accepted_res_not_passed
+   if self.reservation_id == nil
+     errors.add(:id, "can't be reviewed without a reservation")
+   elsif self.reservation.status != "accepted"
+     errors.add(:id, "can't be reviewed while reservation has not been accepted.")
+   elsif self.reservation.checkout > Date.today
+     errors.add(:id, "can't be reviewed before checkout.")
+   end
+ end
+
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
